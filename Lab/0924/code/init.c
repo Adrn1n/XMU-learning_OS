a19fb1d51dbe607cc4ef521cc191b0d909fee0af
Exxon <exxonliu001@yahoo.com>
2025-10-17 03:34:30 +0000

    20251017: Init: Lab/0924: 2

--- Lab/0924/code/init.c
+++ Lab/0924/code/init.c
@@ -7,35 +7,38 @@
 
 #include "sdh.h"
 
-char *argv[] = { "sh", 0 };
+char *argv[] = {"sh", 0};
 
-int
-main(void)
+int main(void)
 {
   int pid, wpid;
 
-  if(open("console", O_RDWR) < 0){
+  if (open("console", O_RDWR) < 0)
+  {
     mknod("console", 1, 1);
     open("console", O_RDWR);
   }
-  dup(0);  // stdout
-  dup(0);  // stderr
+  dup(0); // stdout
+  dup(0); // stderr
 
   printf(1, "Scheduler default policy: %s\n", schedulerName[schedSelected]);
 
-  for(;;){
+  for (;;)
+  {
     printf(1, "init: starting lsh\n");
     pid = fork();
-    if(pid < 0){
+    if (pid < 0)
+    {
       printf(1, "init: fork failed\n");
       exit();
     }
-    if(pid == 0){
+    if (pid == 0)
+    {
       exec(argv[0], argv);
       printf(1, "init: exec sh failed\n");
       exit();
     }
-    while((wpid=wait()) >= 0 && wpid != pid)
+    while ((wpid = wait()) >= 0 && wpid != pid)
       printf(1, "zombie!\n");
   }
 }
